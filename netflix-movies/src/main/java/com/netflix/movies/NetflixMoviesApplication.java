85a3856ba44d9319fd6f885e74643065001c5fb2
siron azevedo <sirondba@gmail.com>
2020-12-20 00:25:49 -0300

    project netflix-movies created

---
+++ netflix-movies/src/main/java/com/netflix/movies/NetflixMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixMoviesApplication.class, args);
+	}
+
+}
