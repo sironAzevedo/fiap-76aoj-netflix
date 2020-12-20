4dfa4eeff4cb7134cd74e89a7e387a4749cddb8d
siron azevedo <sirondba@gmail.com>
2020-12-20 17:13:01 -0300

    Created API User

---
+++ netflix-user/src/main/java/com/netflix/user/NetflixUserApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixUserApplication.class, args);
+	}
+
+}
---
+++ netflix-user/src/test/java/com/netflix/user/NetflixUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
