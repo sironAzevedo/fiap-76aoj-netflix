9ae17543937b8e95e0be9e391a5987409ff0484d
siron azevedo <sirondba@gmail.com>
2020-12-27 14:09:59 -0300

    Project servidor eureka created

---
+++ netflix-eureka-server/src/test/java/com/netflix/eurekaServer/NetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
