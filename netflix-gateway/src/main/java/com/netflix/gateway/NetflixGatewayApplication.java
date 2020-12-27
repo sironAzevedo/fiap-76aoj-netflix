f42b6b1b69c2c80ce7e86a7c09ec3200d1d39afc
siron azevedo <sirondba@gmail.com>
2020-12-27 15:15:58 -0300

    Projeto netflix-gateway created

---
+++ netflix-gateway/src/main/java/com/netflix/gateway/NetflixGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixGatewayApplication.class, args);
+	}
+
+}
---
+++ netflix-gateway/src/test/java/com/netflix/gateway/NetflixGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
