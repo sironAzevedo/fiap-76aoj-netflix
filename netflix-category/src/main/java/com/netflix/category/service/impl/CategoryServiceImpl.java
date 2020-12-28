33b573d551cc596d78f1c25529323a8af6e554b6
siron azevedo <sirondba@gmail.com>
2020-12-28 13:11:19 -0300

    Configrando o server client no projeto netflix-category

--- netflix-category/src/main/java/com/netflix/category/service/impl/CategoryServiceImpl.java
+++ netflix-category/src/main/java/com/netflix/category/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.netflix.category.service.impl;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -18,9 +19,13 @@ public class CategoryServiceImpl implements ICategoryService {
 
 	@Autowired
 	private ICategoryRepository repo;
+	
+	@Value("${test.config}")
+	private String msgConfig;
 
 	@Override
 	public Page<CategoryDTO> findAll(Pageable pageable) {
+		System.out.println("CONFIG = " + msgConfig);
 		Page<CategoryDTO> result = repo.findAll(pageable).map(CategoryConverter.INSTANCE::toDTO);
 		return new PageImpl<>(result.stream().collect(Collectors.toList()), pageable, result.getTotalElements());
 	}
