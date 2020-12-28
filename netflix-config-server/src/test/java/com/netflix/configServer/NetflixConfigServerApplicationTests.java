eae780a5968feff3e18011f5e2c19adf35639ad1
siron azevedo <sirondba@gmail.com>
2020-12-28 12:01:47 -0300

    Projeto de configuracao created

---
+++ netflix-config-server/src/test/java/com/netflix/configServer/NetflixConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
