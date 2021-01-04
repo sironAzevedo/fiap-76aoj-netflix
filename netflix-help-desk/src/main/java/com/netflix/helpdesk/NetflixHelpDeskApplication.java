865a310409235bf5fdf6254ccc381f4244c8bd56
siron azevedo <sirondba@gmail.com>
2021-01-04 03:25:28 -0300

    Create API netflix-help-desk

---
+++ netflix-help-desk/src/main/java/com/netflix/helpdesk/NetflixHelpDeskApplication.java
@@ -0,0 +1,17 @@
+package com.netflix.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class NetflixHelpDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixHelpDeskApplication.class, args);
+	}
+
+}
